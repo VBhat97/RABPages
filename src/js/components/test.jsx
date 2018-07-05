30a4635aca581a81c49a8867c324e1d7559b4106
VBhat97 <vaibhav.bhat2097@gmail.com>
2018-07-05 18:11:43 +0530

    Signup Done

--- src/js/components/test.jsx
+++ src/js/components/test.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 
 var errorStyle = {color : "red"};
+var lowerCaseLetters = /[a-z]/g;
+var upperCaseLetters = /[A-Z]/g;
+var numbers = /[0-9]/g;
+
 
 export default class Signup extends React.Component {
 
@@ -14,6 +18,8 @@ export default class Signup extends React.Component {
             cpasswordValidation : null,
         }
         this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handlecPassword = this.handlecPassword.bind(this);
     }
 
 
@@ -34,6 +40,65 @@ export default class Signup extends React.Component {
         }
     }
 
+    handlePassword(e)
+    {
+        let password = this.refs.password.value;
+
+        if(!password.match(lowerCaseLetters))
+        {
+            this.setState({
+                passwordValidation: "Password must contain a lower case letter",
+            })
+        }
+        else if(!password.match(upperCaseLetters))
+        {
+            this.setState({
+                passwordValidation: "Password must contain an upper case letter",
+            })
+        }
+
+        else if(!password.match(numbers))
+        {
+            this.setState({
+                passwordValidation: "Password must contain atleast a number",
+            })
+        }
+
+        else if(password.length < 8)
+        {
+            this.setState({
+                passwordValidation: "Password must contain minimum 8 characters",
+            })
+        }
+
+        else
+        {
+            this.setState({
+                passwordValidation: null,
+            })
+        }
+
+    }
+
+    handlecPassword(e)
+    {
+        let cpassword = this.state.ref.cpassword;
+
+        if(cpassword===password)
+        {
+            this.setState({
+                cpasswordValidation : null,
+            })
+        }
+
+        else
+        {
+            this.setState({
+                cpasswordValidation: "Passwords do not match",
+            })
+        }
+    }
+
 
   render() {
     return (
@@ -50,10 +115,12 @@ export default class Signup extends React.Component {
                                 <br />
                                 <form class='form-group formwidth'>
                                     <input type='text' placeholder="Name" class='form-control' ref="name" /><br />
-                                    <input type='text' placeholder="Email" class='form-control' ref="email" onChange={this.handleEmail.bind(this)} />
+                                    <input type='text' placeholder="Email" class='form-control' ref="email" onChange={this.handleEmail.bind(this)}/>
                                         <h5 style={errorStyle}>{this.state.emailValidation}</h5>
-                                    <input type="password" placeholder="Password" class='form-control' ref="password" /><br />
-                                    <input type="cpassword" placeholder="Confirm Password" class='form-control' ref="cpassword" /><br />
+                                    <input type="password" placeholder="Password" class='form-control' ref="password" onChange={this.handlePassword.bind(this)}/>
+                                        <h5 style={errorStyle}>{this.state.passwordValidation}</h5>
+                                    <input type="cpassword" placeholder="Confirm Password" class='form-control' ref="cpassword" onChange={this.handlecPassword.bind(this)} /><br />
+                                        <h5 style={errorStyle}>{this.state.cpasswordValidation}</h5>
                                     <input type="submit" value="Signup" class="btn btn-primary"/>
                                 </form>
                             </div>
